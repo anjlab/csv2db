0c6ea9a0ea01f018660c4413f60d16b0631c6887
Dmitry Gusev <dmitry.gusev@gmail.com>
2016-11-08 14:56:58 +0300

    Remove Cygwin conversion from run.sh, pass arguments using `"$@"`

--- src/main/shell/run.sh
+++ src/main/shell/run.sh
@@ -13,17 +13,6 @@ do
     CLASSPATH=$i:$CLASSPATH
 done
 
-# Cygwin conversion
-if [[ "${CLASSPATH}" =~ cygdrive ]]; then
-    CLASSPATH=$(echo "${CLASSPATH}" | sed -e 's#:#;#g;s#/cygdrive/c#C:#gi;s#/#\\#g')
-fi
-for ((i=1;i<10;i++)); do
-    args[$i]=$(eval echo "\$$i")
-    if [[ "${args[$i]}" =~ cygdrive ]]; then
-        args[$i]=$(echo "${args[$i]}" | sed -e 's#:#;#g;s#/cygdrive/c#C:#gi;s#/#\\#g')
-    fi
-done
-
 java $JAVA_OPTS -cp $CLASSPATH \
      com.anjlab.csv2db.Import \
-     "${args[@]}"
+     "$@"
